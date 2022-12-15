1c8258de72f6a7565c415265d0ab72f5130b62c6
Arif <smarifmahmud9@gmail.com>
2022-12-15 06:36:20 +0600

    fourth commit

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template, Response
+import streamlit as st
 import cv2
 import torch
 import easyocr
 from PIL import ImageFont, ImageDraw, Image, ImageGrab
 import numpy as np
+import time
 
-
-app = Flask(__name__)
+frame_window = st.image([])
 camera = cv2.VideoCapture(0)
 
 
@@ -14,8 +14,9 @@ def plate_detection():
     path = 'best.pt'
     model = torch.hub.load('ultralytics/yolov5',
                            'custom', path, force_reload=True)
-
+    frame_window = st.image([])
     capture = cv2.VideoCapture(0)
+    plateholder = st.empty()
 
     while True:
         success, frame = capture.read()
@@ -32,30 +33,36 @@ def plate_detection():
             for string in result:
                 text = text+string[1]+'\n'
 
-            fontpath = "banglamn.ttc"  # <== 这里是宋体路径
-            font = ImageFont.truetype(fontpath, 24)
-            img_pil = Image.fromarray(frame)
-            draw = ImageDraw.Draw(img_pil)
-            b, g, r, a = 221, 82, 6, 0
-            draw.text((50, 80),  text, font=font, fill=(b, g, r, a))
-            frame = np.array(img_pil)
+            plateholder.empty()
+            plateholder.text(text)
+
+            # codes bellow commented for future development
+
+            # fontpath = "banglamn.ttc"  # <== 这里是宋体路径
+            #font = ImageFont.truetype(fontpath, 24)
+            #img_pil = Image.fromarray(frame)
+            #draw = ImageDraw.Draw(img_pil)
+            #b, g, r, a = 221, 82, 6, 0
+            #draw.text((50, 80),  text, font=font, fill=(b, g, r, a))
+            #frame = np.array(img_pil)
+
+            ###############################################
 
             ret, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
+            frame_window.image(frame)
 
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-
-@app.route('/video')
-def video():
-    return Response(plate_detection(), mimetype='multipart/x-mixed-replace; boundary=frame')
+            hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+            st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+try:
+    st.title("Yolo Plate")
+    plate_detection()
+except:
+    pass
