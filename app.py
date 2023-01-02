fe3eb782d5c53a82db74348226092d348e332502
Arif <58362778+Khasnuwr@users.noreply.github.com>
2023-01-03 00:56:11 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from streamlit_webrtc import webrtc_streamer, RTCConfiguration
+import av
 import cv2
 import torch
 import easyocr
@@ -59,7 +61,26 @@ def plate_detection():
      
 
 st.title("Yolo Plate")
-plate_detection()
+#plate_detection()
+
+
+cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+class VideoProcessor:
+	def recv(self, frame):
+		frm = frame.to_ndarray(format="bgr24")
+
+		faces = cascade.detectMultiScale(cv2.cvtColor(frm, cv2.COLOR_BGR2GRAY), 1.1, 3)
+
+		for x,y,w,h in faces:
+			cv2.rectangle(frm, (x,y), (x+w, y+h), (0,255,0), 3)
+
+		return av.VideoFrame.from_ndarray(frm, format='bgr24')
+
 
 #st.title('SOMETHING WENT WRONG ;(')
-
+webrtc_streamer(key="key", video_processor_factory=VideoProcessor,
+				rtc_configuration=RTCConfiguration(
+					{"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+					)
+	)
